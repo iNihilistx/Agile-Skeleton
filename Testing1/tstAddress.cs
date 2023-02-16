244233ecfe7074f706b382eb4558e0741bf84595
Daniel Riley <P2698777@my365.dmu.ac.uk>
2023-02-16 05:32:03 +0000

    made tstAddress

---
+++ Testing1/tstAddress.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstAddress
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock aStock = new clsStock();
+            Assert.ISNotNull(aStock);
+        }
+    }
+}
