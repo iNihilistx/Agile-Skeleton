f890fe0295e2d77280abe868b45702f1705521ed
iNihilist <64751372+iNihilistx@users.noreply.github.com>
2023-02-16 05:47:23 +0000

    added testing

---
+++ Testing1/tstStock.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock aStock = new clsStock();
+            Assert.IsNotNull(aStock);
+        }
+    }
+}
