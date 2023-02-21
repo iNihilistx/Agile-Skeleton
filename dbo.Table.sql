c4b8e182b71b51c3fd7405b79e78971360d7ffe6
Ahmad Inhaam <P2701693@my365.dmu.ac.uk>
2023-02-21 12:33:55 +0000

    first git push

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Staff (
+  StaffID INT PRIMARY KEY,
+  StaffName VARCHAR(255) NOT NULL,
+  DOB DATE NOT NULL,
+  Department VARCHAR(255) NOT NULL,
+  Gender VARCHAR(10) NOT NULL,
+  StaffRole TEXT,
+  AvailableStaff BOOLEAN NOT NULL
+);
