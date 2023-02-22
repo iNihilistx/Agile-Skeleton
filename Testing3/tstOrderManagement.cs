a05beb828189b442b786d6f97596ea22f89414b6
Michael Miller <P2545825@my365.dmu.ac.uk>
2023-02-22 13:54:58 +0000

    intal commit

---
+++ Testing3/tstOrderManagement.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrderManagement
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+        }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrderManagement order = new clsOrderManagement();
+            Assert.IsNotNull(order);
+        }
+
+        
+    }
+}
