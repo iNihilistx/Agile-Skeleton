8f91b0345b4145b2870990a980ef762ba4193224
Michael Miller <P2545825@my365.dmu.ac.uk>
2023-03-06 13:04:52 +0000

    intal unit tests done
    intal unit tests done for testing propertys

--- Testing3/clsOrderManagement.cs
+++ Testing3/clsOrderManagement.cs
@@ -8,5 +8,13 @@ namespace Testing3
 {
     class clsOrderManagement
     {
+        public int OrderId { get;  set; }
+        public string ProductId { get; set; }
+        public string CustomerId { get; set; }
+        public int ProductQuantity { get; set; }
+        public string Status { get; set; }
+        public DateTime OrderDate { get;  set; }
+
+        public bool IsCanceled { get; set; }
     }
 }
--- Testing3/tstOrderManagement.cs
+++ Testing3/tstOrderManagement.cs
@@ -6,12 +6,7 @@ namespace Testing3
     [TestClass]
     public class tstOrderManagement
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-           
-        }
-
+      
         [TestMethod]
         public void InstanceOK()
         {
@@ -19,6 +14,68 @@ namespace Testing3
             Assert.IsNotNull(order);
         }
 
-        
+        [TestMethod]
+        public void OrderIdPropertyOK()
+        {
+            clsOrderManagement order = new clsOrderManagement();
+            int testData = 1;
+            order.OrderId = 1;
+            Assert.AreEqual(order.OrderId, testData);
+        }
+
+        [TestMethod]
+        public void ProductIdPropertyOK()
+        {
+            clsOrderManagement order = new clsOrderManagement();
+            string testData = 1.ToString();
+            order.ProductId = 1.ToString();
+            Assert.AreEqual(order.ProductId, testData);
+        }
+
+
+        [TestMethod]
+        public void CustomerIdPropertyOK()
+        {
+            clsOrderManagement order = new clsOrderManagement();
+            string testData = 1.ToString();
+            order.CustomerId = 1.ToString();
+            Assert.AreEqual(order.CustomerId, testData);
+        }
+
+        [TestMethod]
+        public void ProductQuantityPropertyOK()
+        {
+            clsOrderManagement order = new clsOrderManagement();
+            int testData = 14;
+            order.ProductQuantity = 14;
+            Assert.AreEqual(order.ProductQuantity, testData);
+        }
+
+        [TestMethod]
+        public void StatusPropertyOK()
+        {
+            clsOrderManagement order = new clsOrderManagement();
+            string testData = "In progress";
+            order.Status = testData;
+            Assert.AreEqual(order.Status, testData);
+        }
+
+        [TestMethod]
+        public void OrderDatePropertyOK()
+        {
+            clsOrderManagement order = new clsOrderManagement();
+            DateTime testData = DateTime.Now.Date;
+            order.OrderDate = testData;
+            Assert.AreEqual(order.OrderDate, testData);
+        }
+
+        [TestMethod]
+        public void IsCanceledPropertyOK()
+        {
+            clsOrderManagement order = new clsOrderManagement();
+            bool testData = true;
+            order.IsCanceled = true;
+            Assert.AreEqual(order.IsCanceled, testData);
+        }
     }
 }
